a6d4b1873ccdb902bc2804addd12c8a25123fe2c
marcodabot8-design <marcodabot8@gmail.com>
2026-04-23 16:31:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BELUV_CONFIG = {
-  supabaseUrl: https://xmpmlvsiuuifuewaczas.supabase.co,
-  supabaseAnonKey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtcG1sdnNpdXVpZnVld2FjemFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NTEwNTIsImV4cCI6MjA5MjUyNzA1Mn0.F62SDvMAZxv3nLaK9Fq90ETeLtZHa9LbNoRfoMOrx5M,
+  supabaseUrl: 'https://xmpmlvsiuuifuewaczas.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtcG1sdnNpdXVpZnVld2FjemFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NTEwNTIsImV4cCI6MjA5MjUyNzA1Mn0.F62SDvMAZxv3nLaK9Fq90ETeLtZHa9LbNoRfoMOrx5M',
 };
